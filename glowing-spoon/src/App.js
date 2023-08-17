8f03bd3f23bad1aeff6bd96aa77490e5a4eaadd0
Nathan <feithlicher@gmail.com>
2023-08-18 00:01:36 +0300

    first commit

---
+++ glowing-spoon/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Prompt from './components/Prompt';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Prompt />
+    </div>
+  );
+}
+
+export default App;
---
+++ glowing-spoon/src/components/Prompt.js
@@ -0,0 +1,13 @@
+
+
+const Prompt = () => {
+    return (
+        <div className="promptContainer">
+            <div className="companyName">Remembell</div>
+            <input className="promptInput" placeholder="Query here..." />
+        </div>
+    );
+};
+
+
+export default Prompt;
