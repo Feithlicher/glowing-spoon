f5815c6471475aa3b37756c882ece1ea9db8bbbb
Nathan <feithlicher@gmail.com>
2023-08-22 21:25:02 +0300

    concept of popup

--- glowing-spoon/src/components/Prompt.js
+++ glowing-spoon/src/components/Prompt.js
@@ -1,11 +1,27 @@
-
+import { useState } from 'react';
 
 const Prompt = () => {
+
+    // State to manage popup visibility
+    const [isPopupVisible, setPopupVisibility] = useState(false);
+
+    // Toggle the visibility of the popup
+    const handleButtonClick = () => {
+        setPopupVisibility(!isPopupVisible);
+    }
+
     return (
         <div className="promptContainer">
             <div className="companyName">Remembell</div>
             <input className="promptInput" placeholder="Query here..." />
-            <button className="submitButton">Submit</button>
+            <button className="submitButton" onClick={handleButtonClick}>Submit</button>
+
+            {isPopupVisible && (
+                <div className="popup">
+                    Hello World
+                </div>
+            )}
+
         </div>
     );
 };
